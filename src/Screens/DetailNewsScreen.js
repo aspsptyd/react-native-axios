f677e28cc34a03585a9432160d2b906235a8c26e
aspsptyd <devadiroot@gmail.com>
2023-03-29 14:45:17 +0700

    Strech image in detail

--- src/Screens/DetailNewsScreen.js
+++ src/Screens/DetailNewsScreen.js
@@ -1,12 +1,13 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function DetailNewsScreen({ route, navigation }) {
+    const {width, height} = Dimensions.get('window');
     return (
         <>
             {console.info(route.params.data)}
             <View>
-                <Image style={{ width: 120, height: 120 }} source={{ uri: route.params.data.urlToImage }} />
+                <Image style={{ width: width, height: width }} source={{ uri: route.params.data.urlToImage }} />
                 <Text>{route.params.data.title}</Text>
                 <Text>{route.params.data.author}</Text>
                 <Text>{route.params.data.description}</Text>
